1518b7fd1ceb213dc2aaef6c5542be265e29d0bc
chagneag <chagneag@PC-A104-14.iut-valence.fr>
2013-11-28 12:26:39 +0100

    - ajout recuperation adresse ip locale pour ecoute automatique du serveur sur celle ci, a la place de la mettre en dur

--- JavaProject/src/com/projetjava/srvhttp/HTTPServer.java
+++ JavaProject/src/com/projetjava/srvhttp/HTTPServer.java
@@ -1,31 +1,43 @@
 package com.projetjava.srvhttp;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class HTTPServer{
 	public static void main(String[] args){
 		// args 0 : l'adresse locale en notation pointée
 		// args 1 : le port local
 		
-		InetSocketAddress sa = new InetSocketAddress("127.0.0.1",Integer.parseInt("80"));
-		ServerSocket ss = null;
-		try{
-			ss = new ServerSocket();
-			ss.bind(sa);
-		} catch (IOException e) {
-			System.out.println("erreur creation socket");
-		}
-		
-		while (true){
+		InetAddress thisIp; // on chope l'ip locale du poste pour ecoute sur le reseau
+		try {
+			thisIp = InetAddress.getLocalHost(); 
+			InetSocketAddress sa = new InetSocketAddress(thisIp.getHostAddress(),Integer.parseInt("80"));
+			System.out.println(thisIp.getHostAddress() + ":80");
+
+			ServerSocket ss = null;
 			try{
-				Socket sock = ss.accept();
-				HTTPServerThread serv = new HTTPServerThread(sock);
-				serv.start();
+				ss = new ServerSocket();
+				ss.bind(sa);
 			} catch (IOException e) {
-				System.out.println("erreur creation thread");
+				System.out.println("erreur creation socket");
 			}
-		}
+			
+			while (true){
+				try{
+					Socket sock = ss.accept();
+					HTTPServerThread serv = new HTTPServerThread(sock);
+					serv.start();
+				} catch (IOException e) {
+					System.out.println("erreur creation thread");
+				}
+			}
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		
 	}
 }
