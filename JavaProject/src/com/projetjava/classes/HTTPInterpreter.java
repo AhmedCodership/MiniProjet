d1ffb954207d8d1f0f5d6361de8a799cbdacba46
gueffier <gueffier@PC-A101-16.iut-valence.fr>
2013-12-05 12:11:26 +0100

    Modification du nom de l'interface

--- JavaProject/src/com/projetjava/classes/HTTPInterpreter.java
+++ JavaProject/src/com/projetjava/classes/HTTPInterpreter.java
@@ -1,8 +1,10 @@
 package com.projetjava.classes;
 
-public interface Java_interpreter {
+public interface HTTPInterpreter {
 	//--Fonctions associées aux requètes http
 	public String getIndex();
+	
+	public String getDetails(String param);
 	
 	public String addStudent(String param);
 	
