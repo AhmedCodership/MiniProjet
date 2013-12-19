a8d21470f0cda5f16671657edc9106b6f817cb23
chagneag <chagneag@PC-A105-09.iut-valence.fr>
2013-12-19 12:02:29 +0100

    mise en methode de la fonction decompo args

--- JavaProject/src/com/projetjava/srvhttp/GenerateurFlux.java
+++ JavaProject/src/com/projetjava/srvhttp/GenerateurFlux.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 
 import com.projetjava.classes.HTTPInterpreter;
-import com.projetjava.generatorHTML.generator;
 
 public class GenerateurFlux implements HTTPInterpreter{
 	
@@ -13,48 +12,37 @@ public class GenerateurFlux implements HTTPInterpreter{
 	
 		if ((rq != null) && (rq.startsWith("GET "))){
 			String rqOrig = rq; 
-			rq = rq.substring(rq.indexOf(" ")+1);	// on recoit un "GET /mapage.html HTTP/1.1"
+			/*rq = rq.substring(rq.indexOf(" ")+1);	// on recoit un "GET /mapage.html HTTP/1.1"
 			rq = rq.substring(0, rq.indexOf(" "));	// du coup on decompose pour chopper juste la page
-			rq = rq.substring(1, rq.length());		// on vire le "/" devant le nom de page
+			rq = rq.substring(1, rq.length());		// on vire le "/" devant le nom de page*/
 			
-			if(rq.length() != 0){
+			System.out.println(rq);
+			String param;
+			param = "action";
+			String actionValue = this.getValeurParam(rq,param);
+			System.out.println("actionValue = "+actionValue);
+			param = "id";
+			String idValue = getValeurParam(rq,param);
+			System.out.println(idValue);
+			param = "truc";
+			String trucValue = getValeurParam(rq,param);
+			System.out.println(actionValue);
+			
+			
+			/*if(rq.length() != 0){
 				System.out.println("Decomposition : "+rq);	// affichage pour debug
-			}
+			}*/
 			// virer lecture fichier
 			// decomposer parametres
 			// recuperer chaine
-			if(rq.length() == 0){
+			/*if(rq.length() == 0){
 				System.out.println("Taille de la chaine : "+rq.length());	// affichage pour debug
 				rq = getIndex();
 				System.out.println("Aucune page demandée, on envoie l'index.html");
 				
-			}
+			}*/
 			
-			String listeParam = rqOrig.substring(rqOrig.indexOf("?"),rqOrig.length()); // on choppe les parametres
-			listeParam = listeParam.substring(1,listeParam.indexOf(" "));
-			System.out.println("listeParam : "+listeParam+"\n"); // affichage param
-			String parametreAction = "action=";
-			int action = listeParam.indexOf(parametreAction);
-			System.out.println("Recherche de "+parametreAction+" dans "+listeParam+"...");
-			if(action != -1){
-				System.out.println(parametreAction+" trouvé !");
-				String actionValue = listeParam.substring(listeParam.indexOf(parametreAction)+parametreAction.length(),listeParam.length()); // si le param action existe on supprime la partie "param="
-				System.out.println("Suppression du nom du parametre dans la chaine param ("+listeParam.indexOf(parametreAction)+parametreAction.length()+":"+listeParam.length()+") pour avoir la valeur...");
-				System.out.println("actionValue vaut maintenant : "+actionValue);
-				int autreParam = actionValue.indexOf("&");
-				if(autreParam == -1){
-					System.out.println("Autre parametre non trouvé...");
-					// si on trouve pas de &, la liste de param est terminée donc le param page = totalité de la chaine
-					System.out.println("Pas d'autre param, value : "+actionValue);
-				}
-				else{
-					System.out.println("Autre parametre trouvé !! ");
-					System.out.println("on tronque actionValue sur 0 à &");
-					actionValue = actionValue.substring(0,actionValue.indexOf("&")); // si on trouve &, la liste de param est pas terminée donc le param page = 0 à recherche &
-				}
-				System.out.println("value : "+actionValue+"\n\n\n");
-			}
-			//System.out.println(page); // affichage param
+			
 			
 			
 			/*File f = new File(rq);
@@ -73,6 +61,40 @@ public class GenerateurFlux implements HTTPInterpreter{
 		
 	}
 
+	public String getValeurParam(String rq, String parametreARechercher){
+		if(rq.indexOf("?") != -1){
+			System.out.println("rq.substring(rq.lenght : "+rq.length()+", rq.indexOf(?) :"+rq.indexOf("?"));
+			String listeParam = rq.substring(rq.indexOf("?"),rq.length()); // on choppe les parametres
+			listeParam = listeParam.substring(1,listeParam.indexOf(" "));
+			System.out.println("listeParam : "+listeParam+"\n"); // affichage param
+			parametreARechercher = parametreARechercher + "=";
+			int action = listeParam.indexOf(parametreARechercher);
+			System.out.println("Recherche de "+parametreARechercher+" dans "+listeParam+"...");
+			String paramValue = null;
+			if(action != -1){
+				System.out.println(parametreARechercher+" trouvé !");
+				paramValue = listeParam.substring(listeParam.indexOf(parametreARechercher)+parametreARechercher.length(),listeParam.length()); // si le param action existe on supprime la partie "param="
+				System.out.println("Suppression du nom du parametre dans la chaine param ("+listeParam.indexOf(parametreARechercher)+parametreARechercher.length()+":"+listeParam.length()+") pour avoir la valeur...");
+				System.out.println("paramValue vaut maintenant : "+paramValue);
+				int autreParam = paramValue.indexOf("&");
+				if(autreParam == -1){
+					System.out.println("Autre parametre non trouvé...");
+					// si on trouve pas de &, la liste de param est terminée donc le param page = totalité de la chaine
+					System.out.println("Pas d'autre param, value : "+paramValue);
+				}
+				else{
+					System.out.println("Autre parametre trouvé !! ");
+					System.out.println("on tronque paramValue sur 0 à &");
+					paramValue = paramValue.substring(0,paramValue.indexOf("&")); // si on trouve &, la liste de param est pas terminée donc le param page = 0 à recherche &
+				}
+				System.out.println("value : "+paramValue+"\n\n\n");
+			}
+			return paramValue;
+		}else{
+			System.out.println("Pas de ?");
+			return "PAs de ?";
+		}
+	}
 	@Override
 	public String getIndex() {
 		// TODO Auto-generated method stub
